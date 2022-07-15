77106947b6204ae4fe8c17734c5f4ccff150441d
Andris Potrebko <andris.potrebko@gmail.com>
2022-07-15 14:09:58 +0200

    Added test for L5 corrections. Made it possible to run from command line

--- CoffeaJERCProcessor_L5.py
+++ CoffeaJERCProcessor_L5.py
@@ -1,13 +1,10 @@
 #CoffeaJERCProcessor.py
 
-import copy
 import scipy.stats as ss
 from coffea import hist, processor, nanoevents
 from coffea import util
 import numpy as np
-import itertools
 import pandas as pd
-from numpy.random import RandomState
 from coffea.jetmet_tools import FactorizedJetCorrector, JetCorrectionUncertainty
 from coffea.jetmet_tools import JECStack, CorrectedJetsFactory
 from coffea.lookup_tools import extractor
@@ -16,8 +13,8 @@ from coffea.lookup_tools import extractor
 
 import awkward as ak
 #from coffea.nanoevents.methods import nanoaod
-from coffea.nanoevents.methods import candidate
-from coffea.nanoevents.methods import vector
+# from coffea.nanoevents.methods import candidate
+# from coffea.nanoevents.methods import vector
 
 
 
@@ -26,17 +23,7 @@ manual_bins = [400, 500, 600, 800, 1000, 1500, 2000, 3000, 7000, 10000]
 ptbins = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 20, 23, 27, 30, 35, 40, 45, 57, 72, 90, 120, 
         150, 200, 300, 400, 550, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 10000 ])
 
-
-etabins =   np.array([-4.889,  -4.716,  -4.538,  -4.363,  -4.191,  -4.013,  -3.839,  -3.664,  -3.489,
-           -3.314,  -3.139,  -2.964,  -2.853,  -2.65,  -2.5,  -2.322,  -2.172,  -2.043,  -1.93,  -1.83,
-           -1.74,  -1.653,  -1.566,  -1.479,  -1.392,  -1.305,  -1.218,  -1.131,  -1.044,  -0.957,  -0.879,
-           -0.783,  -0.696,  -0.609,  -0.522,  -0.435,  -0.348,  -0.261,  -0.174,  -0.087,  0,  0.087,  0.174,
-           0.261,  0.348,  0.435,  0.522,  0.609,  0.696,  0.783,  0.879,  0.957,  1.044,  1.131,  1.218,
-           1.305,  1.392,  1.479,  1.566,  1.653,  1.74,  1.83,  1.93,  2.043,  2.172,  2.322,  2.5,  2.65,
-           2.853,  2.964,  3.139,  3.314,  3.489,  3.664,  3.839,  4.013,  4.191,  4.363,  4.538,  4.716,
-           4.889, 5.191 ])
-
-
+etabins = np.array([-5, -3, -2.5, -1.3, 0, 1.3, 2.5, 3, 5])
 
 
 """@TTbarResAnaHadronic Package to perform the data-driven mistag-rate-based ttbar hadronic analysis. 
@@ -51,39 +38,27 @@ class Processor(processor.ProcessorABC):
         jetpt_axis = hist.Bin("pt", r"$p_T$", ptbins)
         ptresponse_axis = hist.Bin("ptresponse", "RECO / GEN response", 100, 0, 5)
         jetmass_axis = hist.Bin("jetmass", r"Jet $m$ [GeV]", 50, 0, 500)
-        #jetpt_axis = hist.Bin("jetpt", r"Jet $p_{T}$ [GeV]", 50, 0, 5000)
-        ttbarmass_axis = hist.Bin("ttbarmass", r"$m_{t\bar{t}}$ [GeV]", 50, 0, 5000)
         jeteta_axis = hist.Bin("jeteta", r"Jet $\eta$", etabins)
         jetphi_axis = hist.Bin("jetphi", r"Jet $\phi$", 50, -np.pi, np.pi)
-        jety_axis = hist.Bin("jety", r"Jet $y$", 50, -3, 3)
-        jetdy_axis = hist.Bin("jetdy", r"Jet $\Delta y$", 50, 0, 5)
-        manual_axis = hist.Bin("jetp", r"Jet Momentum [GeV]", manual_bins)
-        tagger_axis = hist.Bin("tagger", r"deepTag", 50, 0, 1)
-        tau32_axis = hist.Bin("tau32", r"$\tau_3/\tau_2$", 50, 0, 2)
-        
-        subjetmass_axis = hist.Bin("subjetmass", r"SubJet $m$ [GeV]", 50, 0, 500)
-        subjetpt_axis = hist.Bin("subjetpt", r"SubJet $p_{T}$ [GeV]", 50, 0, 2000)
-        subjeteta_axis = hist.Bin("subjeteta", r"SubJet $\eta$", 50, -4, 4)
-        subjetphi_axis = hist.Bin("subjetphi", r"SubJet $\phi$", 50, -np.pi, np.pi)
 
-        self._accumulator = processor.dict_accumulator({
-            'ptresponse': hist.Hist("Counts", dataset_axis, jetpt_axis, jeteta_axis, ptresponse_axis),
-            'jetmass': hist.Hist("Counts", dataset_axis, cats_axis, jetmass_axis),
-            'jetpt':     hist.Hist("Counts", dataset_axis, jetpt_axis),
-            'jeteta':    hist.Hist("Counts", dataset_axis, jeteta_axis),
-            'jetphi':    hist.Hist("Counts", dataset_axis, jetphi_axis),
-            'cutflow': processor.defaultdict_accumulator(int),
-            
-        })
+        subsamples = ['b', 'c', 'l', 'g']
+        acc_dict = {'ptresponse_'+samp: hist.Hist("Counts", dataset_axis, jetpt_axis, jeteta_axis, ptresponse_axis) for samp in subsamples}
+        acc_dict['ptresponse']  = hist.Hist("Counts", dataset_axis, jetpt_axis, jeteta_axis, ptresponse_axis)
+        acc_dict['jetpt']       = hist.Hist("Counts", dataset_axis, jetpt_axis)
+        acc_dict['jeteta']      = hist.Hist("Counts", dataset_axis, jeteta_axis)
+        acc_dict['cutflow']     = processor.defaultdict_accumulator(int)
+
+        self._accumulator = processor.dict_accumulator(acc_dict)
         
         
         ext = extractor()
         ext.add_weight_sets([
             "* * Summer19UL18_V5_MC/Summer19UL18_V5_MC_L1FastJet_AK4PFchs.txt",
+            "* * Summer19UL18_V5_MC/Summer19UL18_V5_MC_L2Relative_AK4PFchs.txt"
         ])
         ext.finalize()
 
-        jec_stack_names = ["Summer19UL18_V5_MC_L1FastJet_AK4PFchs"]
+        jec_stack_names = ["Summer19UL18_V5_MC_L1FastJet_AK4PFchs", "Summer19UL18_V5_MC_L2Relative_AK4PFchs"]
 
         evaluator = ext.make_evaluator()
         
@@ -99,6 +74,7 @@ class Processor(processor.ProcessorABC):
         
         self.corrector = FactorizedJetCorrector(
             Summer19UL18_V5_MC_L1FastJet_AK4PFchs=evaluator['Summer19UL18_V5_MC_L1FastJet_AK4PFchs'],
+            Summer19UL18_V5_MC_L2Relative_AK4PFchs=evaluator['Summer19UL18_V5_MC_L2Relative_AK4PFchs'],
         )
 
         self.name_map = jec_stack.blank_name_map
@@ -131,7 +107,6 @@ class Processor(processor.ProcessorABC):
         # Event Cuts
         
         
-        
         # apply npv cuts
         npvCut = (events.PV.npvsGood > 0)
         pvzCut = (np.abs(events.PV.z) < 24)
@@ -139,8 +114,6 @@ class Processor(processor.ProcessorABC):
         
         selectedEvents = events[npvCut & pvzCut & rxyCut]
 
-        
-        
         # get GenJets and Jets
         GenJets = selectedEvents.GenJet[:,0:2]
         jets = selectedEvents.Jet
@@ -158,7 +131,7 @@ class Processor(processor.ProcessorABC):
         corrected_jets = self.jet_factory.build(jets, lazy_cache=events_cache)
 
         jetpt = jets.pt
-        corrected_jetpt = corrected_jets.pt
+        # corrected_jetpt = corrected_jets.pt
 
         # MC jet matching
         matchedJets = ak.cartesian([GenJets, corrected_jets])
@@ -179,7 +152,38 @@ class Processor(processor.ProcessorABC):
         
         
         ptresponse = jetpt / matched_genjetpt
+
+        subsamples = ['b', 'c', 'l', 'g']
+        masks = {}
+        masks['b'] = (matchedJets.slot0.partonFlavour==5) | (matchedJets.slot0.partonFlavour==-5)
+        masks['c'] = (matchedJets.slot0.partonFlavour==4) | (matchedJets.slot0.partonFlavour==-4)
+        masks['l'] = ((matchedJets.slot0.partonFlavour==1) | (matchedJets.slot0.partonFlavour==-1) |
+                     (matchedJets.slot0.partonFlavour==2) | (matchedJets.slot0.partonFlavour==-2) |
+                    (matchedJets.slot0.partonFlavour==3) | (matchedJets.slot0.partonFlavour==-3) )
+        masks['g'] = (matchedJets.slot0.partonFlavour==21)
+
+        # b_pos = (matchedJets.slot0.partonFlavour==5) | (matchedJets.slot0.partonFlavour==-5)
+        # c_pos = (matchedJets.slot0.partonFlavour==4) | (matchedJets.slot0.partonFlavour==-4)
+        # l_pos = ((matchedJets.slot0.partonFlavour==1) | (matchedJets.slot0.partonFlavour==-1) |
+        #          (matchedJets.slot0.partonFlavour==2) | (matchedJets.slot0.partonFlavour==-2) |
+        #          (matchedJets.slot0.partonFlavour==3) | (matchedJets.slot0.partonFlavour==-3) )        
+        # g_pos = matchedJets.slot0.partonFlavour==21
+
+
+        ptresponses            = { sample: ptresponse[masks[sample]]             for sample in subsamples }
+        matched_genjetpts      = { sample: matched_genjetpt[masks[sample]]       for sample in subsamples }
+        matched_genjetetas     = { sample: matched_genjeteta[masks[sample]]      for sample in subsamples }
+
+        # for sample in sumsamples:
+            # ptresponses
         
+        # ptresponse_b = ptresponse[b_pos]
+        # ptresponse_c = ptresponse[c_pos]
+        # ptresponse_l = ptresponse[l_pos]
+        # ptresponse_g = ptresponse[g_pos]
+
+
+
         output['jetpt'].fill(dataset = dataset, 
                         pt = ak.flatten(matched_genjetpt))
 
@@ -190,7 +194,11 @@ class Processor(processor.ProcessorABC):
         output['ptresponse'].fill(dataset=dataset, pt=ak.to_numpy(ak.flatten(matched_genjetpt), allow_missing=True),
                         jeteta=ak.to_numpy(ak.flatten(matched_genjeteta), allow_missing=True),
                         ptresponse=ak.to_numpy(ak.flatten(ptresponse), allow_missing=True))
-        
+        # subsamples = ['b', 'c', 'l', 'g']
+        for sample in subsamples:
+               output['ptresponse_'+sample].fill(dataset=dataset, pt=ak.to_numpy(ak.flatten(matched_genjetpts[sample]), allow_missing=True),
+                        jeteta=ak.to_numpy(ak.flatten(matched_genjetetas[sample]), allow_missing=True),
+                        ptresponse=ak.to_numpy(ak.flatten(ptresponses[sample] ), allow_missing=True))
         
         return output
 
